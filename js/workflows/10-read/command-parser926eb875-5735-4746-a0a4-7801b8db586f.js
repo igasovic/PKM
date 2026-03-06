efe33add0df1265f0168a5368d54de9aa896dad0
htec-igorg <igor.gasovic@htecgroup.com>
2026-03-05 21:12:34 -0600

    fix

---
+++ js/workflows/10-read/command-parser926eb875-5735-4746-a0a4-7801b8db586f.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const fn = require("./command-parser__926eb875-5735-4746-a0a4-7801b8db586f.js");
+
+module.exports = async function bridge(ctx) {
+  return fn(ctx);
+};
---
+++ js/workflows/10-read/format-telegram-messagef305ac84-35d3-44df-8ef5-1c0e004f37b8.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const fn = require("./format-telegram-message__f305ac84-35d3-44df-8ef5-1c0e004f37b8.js");
+
+module.exports = async function bridge(ctx) {
+  return fn(ctx);
+};
